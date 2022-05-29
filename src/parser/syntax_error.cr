8caee987a73b0281a170874986eda34ffcb745b1
I3oris <boris31.resch@gmail.com>
2022-05-29 13:05:48 +0200

    Improve show-in-source algorithm.

--- src/parser/syntax_error.cr
+++ src/parser/syntax_error.cr
@@ -1,32 +1,21 @@
 abstract class Let::Parser < Let::CharReader
   # TODO: docs
   class SyntaxError < Exception
-    def initialize(@message : String, @location : Location, @string : String)
+    def initialize(@message : String, @source : String, @begin_location : Location, @end_location : Location = begin_location)
     end
 
-    # TODO: docs
-    def show_location(io)
-      line_number, line_pos = @location.line_number, @location.line_pos
-      io << "at [#{line_number}:#{line_pos}]\n"
+    def to_s(io)
+      io << message << "\n"
 
-      lines = @string.split('\n')
-      start = {0, line_number - 2}.max
-      end_ = {line_number + 2, lines.size}.min
-      return if start > end_
-
-      lines[start..end_].each_with_index do |l, i|
-        io << l << '\n'
-        if start + i == line_number
-          line_pos.times { io << ' ' }
-          io << "^\n"
-        end
-      end
+      io << "At [#{@begin_location.line_number}:#{@begin_location.line_pos}]:\n"
+      @begin_location.show_in_source(io, @source, end_location: @end_location)
     end
 
     def inspect_with_backtrace(io)
       io << message << " (" << self.class << ")\n"
 
-      show_location(io)
+      io << "At [#{@begin_location.line_number}:#{@begin_location.line_pos}]\n"
+      @begin_location.show_in_source(io, @source, end_location: @end_location)
 
       backtrace?.try &.each do |frame|
         io.print "  from "
@@ -43,7 +32,7 @@ abstract class Let::Parser < Let::CharReader
   end
 
   # TODO: docs
-  def raise_syntax_error(message, location = self.location, string = self.string)
-    raise SyntaxError.new message, location, string
+  def raise_syntax_error(message : String, begin_location : Location = self.location, end_location : Location = begin_location, source : String = self.string)
+    raise SyntaxError.new message, source, begin_location, end_location
   end
 end
