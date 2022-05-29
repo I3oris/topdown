0bd804e2a48939cdbd4b49727634204f12246a2b
I3oris <boris31.resch@gmail.com>
2022-05-29 19:36:19 +0200

    Write spec for `Location`.

---
+++ spec/location_spec.cr
@@ -0,0 +1,85 @@
+require "./spec_helper"
+
+zero = TopDown::Location.new(0, 0, 0)
+l1 = TopDown::Location.new(5, line_number: 0, line_pos: 5)
+l2 = TopDown::Location.new(17, line_number: 0, line_pos: 17)
+l3 = TopDown::Location.new(32, line_number: 1, line_pos: 13)
+
+source = <<-SOURCE
+  puts "Hello World"
+  puts "Hello 💎"
+  puts "Hello ♥"
+  SOURCE
+
+describe TopDown::Location do
+  it "show in source l1" do
+    output = String.build do |io|
+      l1.show_in_source(io, source)
+    end
+
+    output.should eq <<-OUTPUT
+         0 | puts "Hello World"
+                  ^
+         1 | puts "Hello 💎"
+         2 | puts "Hello ♥"\n
+      OUTPUT
+  end
+
+  it "show in source l1-l2" do
+    output = String.build do |io|
+      l1.show_in_source(io, source, end_location: l2)
+    end
+
+    output.should eq <<-OUTPUT
+         0 | puts "Hello World"
+                  ^~~~~~~~~~~~
+         1 | puts "Hello 💎"
+         2 | puts "Hello ♥"\n
+      OUTPUT
+  end
+
+  it "show in source l1-l3" do
+    output = String.build do |io|
+      l1.show_in_source(io, source, end_location: l3)
+    end
+
+    output.should eq <<-OUTPUT
+         0 | puts "Hello World"
+                  ^~~~~~~~~~~~~
+         1 | puts "Hello 💎"
+             ~~~~~~~~~~~~~
+         2 | puts "Hello ♥"\n
+      OUTPUT
+  end
+
+  it "show in source zero-l1" do
+    output = String.build do |io|
+      zero.show_in_source(io, source, end_location: l1)
+    end
+
+    output.should eq <<-OUTPUT
+         0 | puts "Hello World"
+             ^~~~~
+         1 | puts "Hello 💎"
+         2 | puts "Hello ♥"\n
+      OUTPUT
+  end
+
+  it "substract" do
+    (l1 - zero).should eq l1
+    (l2 - l1).should eq TopDown::Location.new(12, line_number: 0, line_pos: 12)
+    (l3 - l2).should eq TopDown::Location.new(15, line_number: 1, line_pos: -4)
+  end
+
+  it "compare" do
+    (l1 <=> l2).should eq -1
+    (zero < l1 < l2 < l3).should be_true
+    (l1 > l3).should be_false
+
+    l2.in?(l1..l3).should be_true
+    l1.in?(l2..l3).should be_false
+
+    l3.in?(l1..l3).should be_true
+    l3.in?(l1...l3).should be_false
+  end
+end
