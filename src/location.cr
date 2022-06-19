29152e421b40afe9114a4bd68f40f145e8200118
I3oris <boris31.resch@gmail.com>
2022-06-19 03:35:29 +0200

    Write docs for `Location`.

--- src/location.cr
+++ src/location.cr
@@ -1,11 +1,44 @@
-# TODO: docs
+# Represents a location into a parser source.
+#
+# `pos` is the position on string (in number of bytes).
+#
+# `line_number` start by 0.
+#
+# `line_pos` is the position on line (in number of characters).
 record TopDown::Location,
   pos : Int32,
   line_number : Int32,
   line_pos : Int32 do
   include Comparable(Location)
 
-  # TODO: docs
+  # Displays this location in *source*.
+  #
+  # * *io* : the output io.
+  # * *source* : the source string.
+  # * *radius* : how many lines above and below to show.
+  # * *begin_location* : if set other than `self`, the function show the range location between `self` and *begin_location*.
+  #
+  # ```
+  # l1 = TopDown::Location.new(5, line_number: 0, line_pos: 5)
+  # l2 = TopDown::Location.new(17, line_number: 0, line_pos: 17)
+  # source = <<-SOURCE
+  #   puts "Hello World"
+  #   puts "Hello 💎"
+  #   puts "Hello ♥"
+  #   SOURCE
+  #
+  # l1.show_in_source(STDOUT, source)
+  # #   0 | puts "Hello World"
+  # #            ^
+  # #   1 | puts "Hello 💎"
+  # #   2 | puts "Hello ♥"\n
+  #
+  # l2.show_in_source(STDOUT, source, begin_location: l1)
+  # #   0 | puts "Hello World"
+  # #            ^~~~~~~~~~~~
+  # #   1 | puts "Hello 💎"
+  # #   2 | puts "Hello ♥"\n
+  # ```
   def show_in_source(io : IO, source : String, radius : Int32 = 2, begin_location : Location = self)
     diff_location = self - begin_location
 
@@ -42,22 +75,33 @@ record TopDown::Location,
     end
   end
 
-  # TODO: docs
-  def cursor_char(n)
+  private def cursor_char(n)
     n == 0 ? '^' : '~'
   end
 
-  # TODO: docs
-  def line_prelude(line_number)
+  private def line_prelude(line_number)
     line_number.to_s.rjust(4) + " | "
   end
 
-  # TODO: docs
+  # Gives the relative location between two others.
+  #
+  # NOTE: `line_pos` may be negative even `self` > `other`.
+  #
+  # ```
+  # l1 = TopDown::Location.new(5, line_number: 0, line_pos: 5)
+  # l2 = TopDown::Location.new(17, line_number: 0, line_pos: 17)
+  # l3 = TopDown::Location.new(32, line_number: 1, line_pos: 13)
+  #
+  # l2 - l1 # => TopDown::Location(@pos=12, @line_number=0, @line_pos=12)
+  # l3 - l2 # => TopDown::Location(@pos=15, @line_number=1, @line_pos=-4)
+  # ```
   def -(other : Location)
     Location.new(@pos - other.pos, @line_number - other.line_number, @line_pos - other.line_pos)
   end
 
-  # TODO: docs
+  # Compares two locations by their `pos` only.
+  #
+  # Returns `self.pos <=> other.pos`.
   def <=>(other : Location)
     @pos <=> other.pos
   end
